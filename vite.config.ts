12f1195dab32577a9bae08bf8fb95ca6ff8d76f5
Lindomar Lima <lsantosmanga21@gmail.com>
2025-06-17 21:11:03 -0300

    STRUCT | Adicionando pipeline de testes

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,10 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
+    coverage: {
+      provider: 'v8',
+      reportsDirectory: 'coverage',
+      reporter: ['text', 'lcov'],
+    },
   },
 })
